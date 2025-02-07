372c46390b17d463ea794c303d7b79f7a70d6c6a
winlogon <walker84837@gmail.com>
2025-02-07 18:45:15 +0100

    feat: split file manager into separate module

--- src/file_manager.rs
+++ src/file_manager.rs
@@ -1,8 +1,9 @@
-use crate::note_manager::NoteManager;
+use crate::{args::Args, note_manager::NoteManager};
 use anyhow::{Context, Result};
 use log::info;
 use std::fs::File;
 use std::io::{Read, Write};
+use std::path::PathBuf;
 use std::process::Command;
 use uuid::Uuid;
 
@@ -21,7 +22,8 @@ impl FileManager {
     /// Loads and decrypts the content of a note
     pub fn load_and_decrypt_note_content(filename: &str, pin: &str) -> Result<Vec<u8>> {
         let mut encrypted_data = Vec::new();
-        let mut file = File::open(filename).context("Unable to open file")?;
+        let mut file =
+            File::open(filename).with_context(|| format!("Unable to open file: {}", filename))?; // Include filename in error
         file.read_to_end(&mut encrypted_data)
             .context("Unable to read file")?;
 
@@ -36,8 +38,9 @@ impl FileManager {
     }
 
     /// Opens the file in the default text editor
-    pub fn open_in_editor(filename: &str) -> Result<()> {
-        let editor = std::env::var("EDITOR").unwrap_or_else(|_| "nano".to_string());
+    pub fn open_in_editor(args: &Args, filename: PathBuf) -> Result<()> {
+        let env_editor = std::env::var("EDITOR").unwrap_or_else(|_| "nano".to_string());
+        let editor = args.editor.as_ref().unwrap_or(&env_editor);
         Command::new(editor).arg(filename).spawn()?.wait()?;
         Ok(())
     }

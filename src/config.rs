21489ddd0f3e6a360c8531bfc984e16e64b5ded3
winlogon <walker84837@gmail.com>
2025-02-07 18:45:02 +0100

    feat: implement Config struct with default values

--- src/config.rs
+++ src/config.rs
@@ -1,39 +1,85 @@
+use log::{error, warn};
 use serde_derive::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
+use thiserror::Error;
 
-#[derive(Default, Serialize, Deserialize)]
+const NOTES_FOLDER: &str = "notes";
+
+#[derive(Serialize, Deserialize)]
 pub struct Config {
-    pub pin_hash: Option<String>,
-    pub notes_dir: Option<String>,
+    pub pin_hash: String,
+    pub notes_dir: String,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        fs::create_dir_all(NOTES_FOLDER).unwrap();
+        Self {
+            pin_hash: "".to_string(),
+            notes_dir: NOTES_FOLDER.to_string(),
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum FileError {
+    #[error("Failed to write file: {0}")]
+    WriteError(String),
+    #[error("Failed to serialize config: {0}")]
+    SerializeError(String),
 }
 
 impl Config {
-    pub fn new() -> Self {
-        let config_path = Self::config_file_path();
-        if config_path.exists() {
-            let config_str = std::fs::read_to_string(config_path).unwrap_or_default();
-            toml::from_str(&config_str).unwrap_or_default()
-        } else {
-            Self {
-                pin_hash: None,
-                notes_dir: None,
+    pub fn new(config_path: &Option<PathBuf>) -> Config {
+        let path = match config_path.to_owned() {
+            Some(p) => p,
+            None => Self::config_file_path(),
+        };
+
+        if !path.exists() {
+            return Config::default();
+        }
+        match std::fs::read_to_string(path) {
+            Ok(config_str) => Self::parse_config(config_str),
+            Err(e) => {
+                error!("Error while reading the configuration: {e}");
+                warn!("Using default configuration");
+                Config::default()
             }
         }
     }
 
-    pub fn save(&self) {
-        let config_str = toml::to_string(self).expect("Failed to serialize config");
-        std::fs::write(Self::config_file_path(), config_str).expect("Unable to write config file");
+    /// Parse the TOML config from a string.
+    /// Returns default configuration if it fails to parse.
+    fn parse_config(config_str: String) -> Config {
+        match toml::from_str(&config_str) {
+            Ok(config) => config,
+            Err(e) => {
+                error!("Error while parsing the configuration: {e}");
+                warn!("Using default configuration");
+                Config::default()
+            }
+        }
     }
 
-    pub fn load_notes_dir() -> Option<String> {
-        let config = Config::new();
-        config.notes_dir
+    /// Save the config to a file
+    pub fn save(&self) -> Result<(), FileError> {
+        let config_str =
+            toml::to_string(self).map_err(|e| FileError::SerializeError(e.to_string()))?;
+        let config_path = Self::config_file_path();
+
+        std::fs::create_dir_all(config_path.parent().unwrap())
+            .map_err(|e| FileError::WriteError(e.to_string()))?;
+
+        std::fs::write(config_path, config_str)
+            .map_err(|e| FileError::WriteError(e.to_string()))?;
+        Ok(())
     }
 
     fn config_file_path() -> PathBuf {
         let mut config_path = dirs::config_dir().unwrap();
-        config_path.push("notes-renamer.toml");
+        config_path.push("cryptnote");
+        config_path.push("cryptnote.toml");
         config_path
     }
 }

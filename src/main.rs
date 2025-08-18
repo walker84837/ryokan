404fe661b96b811e67d73b6f7250e84f24ef4472
winlogon <walker84837@gmail.com>
2025-08-18 04:44:15 +0200

    refactor(tui): move tui logic to different module
    Code changes also include moving code to show and hide cursor to a macro

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,12 @@ mod note_database;
 mod pin;
 
 use crate::{args::Args, config::Config};
-use anyhow::{Context, Result};
+use anyhow::Result;
 use clap::Parser;
-use crossterm::{
-    event::{self, Event, KeyCode},
-    execute,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
-};
+mod tui;
+
 use log::LevelFilter;
-use ratatui::{
-    Terminal,
-    backend::CrosstermBackend,
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::{Line, Span},
-    widgets::{Block, Borders, List, ListItem, ListState, Paragraph},
-};
-use std::{
-    fs, io,
-    path::{Path, PathBuf},
-};
-use uuid::Uuid;
+use std::{fs, path::Path};
 
 fn main() -> Result<()> {
     let args = Args::parse();
@@ -69,165 +54,7 @@ fn main() -> Result<()> {
 
     let mut note_database = note_database::NoteDatabase::new(&args.config_file);
 
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    let notes_dir = &config.notes_dir;
-    let mut notes = fs::read_dir(&notes_dir)?
-        .filter_map(|entry| entry.ok())
-        // TODO: take the ones that don't match and convert them
-        .filter(|entry| entry.file_name().to_string_lossy().ends_with(".enc.txt"))
-        .collect::<Vec<_>>();
-
-    let mut list_state = ListState::default();
-    let mut selected = 0;
-    list_state.select(Some(selected));
-    loop {
-        draw_terminal(&mut terminal, &mut notes, &mut list_state, &pin)?;
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => break,
-                KeyCode::Char('n') => {
-                    let notes_dir_path = PathBuf::from(notes_dir.clone());
-                    let filename = file::generate_uuid_filename();
-                    let path = notes_dir_path.join(&filename);
-                    let encrypted_content = note::encrypt_note_content(&Vec::new(), &pin)?;
-                    file::save_note_to_file(&encrypted_content, &path.to_string_lossy())?;
-
-                    notes = fs::read_dir(notes_dir_path)?
-                        .filter_map(|entry| entry.ok())
-                        .filter(|entry| entry.file_name().to_string_lossy().ends_with(".enc.txt"))
-                        .collect();
-                }
-                KeyCode::Down => {
-                    if selected < notes.len().saturating_sub(1) {
-                        selected += 1;
-                        list_state.select(Some(selected));
-                    }
-                }
-                KeyCode::Up => {
-                    if selected > 0 {
-                        selected -= 1;
-                        list_state.select(Some(selected));
-                    }
-                }
-                KeyCode::Enter => {
-                    // TODO: move to separate function
-                    if let Some(selected) = list_state.selected()
-                        && let Some(note) = notes.get(selected)
-                    {
-                        let temp_file = format!("temp_{}.txt", Uuid::new_v4());
-                        let note_path = note.path().to_string_lossy().into_owned();
-                        let decrypted_content =
-                            file::load_and_decrypt_note_content(&note_path, &pin)?;
-                        file::save_note_to_file(&decrypted_content, &temp_file)
-                            .context("Error saving note to temporary file")?;
-
-                        // restore terminal to normal mode and main screen before launching editor
-                        control_tui(&mut terminal, false)?;
-
-                        // re-enable raw mode and alternate screen after editor exits
-                        control_tui(&mut terminal, true)?;
-                        draw_terminal(&mut terminal, &mut notes, &mut list_state, &pin)?;
-
-                        let encrypted_content =
-                            note::encrypt_note_content(&decrypted_content, &pin)?;
-
-                        file::save_note_to_file(&encrypted_content, &note_path)?;
-                        fs::remove_file(temp_file)?;
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-    Ok(())
-}
-
-fn control_tui(
-    terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
-    show_cursor: bool,
-) -> Result<()> {
-    if show_cursor {
-        execute!(terminal.backend_mut(), EnterAlternateScreen)?;
-        terminal.show_cursor()?;
-    } else {
-        execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
-        terminal.hide_cursor()?;
-    }
-    Ok(())
-}
-
-/// Draws the terminal UI
-fn draw_terminal(
-    terminal: &mut Terminal<CrosstermBackend<io::Stdout>>,
-    notes: &mut [fs::DirEntry],
-    list_state: &mut ListState,
-    pin: &str,
-) -> Result<()> {
-    terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(70),
-                    Constraint::Percentage(20),
-                    Constraint::Min(3),
-                ]
-                .as_ref(),
-            )
-            .split(f.area());
-
-        let items: Vec<_> = notes
-            .iter()
-            .map(|note| {
-                let filename = note.file_name().to_string_lossy().to_string();
-                ListItem::new(filename)
-            })
-            .collect();
-        let notes_list = List::new(items)
-            .block(Block::default().borders(Borders::ALL).title("Notes"))
-            .highlight_style(
-                Style::default()
-                    .fg(Color::Yellow)
-                    .add_modifier(Modifier::BOLD),
-            )
-            .highlight_symbol(">> ");
-        f.render_stateful_widget(notes_list, chunks[0], list_state);
-
-        let preview = if let Some(selected) = list_state.selected()
-            && let Some(note) = notes.get(selected)
-        {
-            let filename = note.path();
-            match file::load_and_decrypt_note_content(filename.to_string_lossy().as_ref(), pin) {
-                Ok(content) => String::from_utf8_lossy(&content).to_string(),
-                Err(_) => "Error reading note.".to_string(),
-            }
-        } else {
-            "No note selected.".to_string()
-        };
-
-        let preview_paragraph =
-            Paragraph::new(preview).block(Block::default().borders(Borders::ALL).title("Preview"));
-        f.render_widget(preview_paragraph, chunks[1]);
-
-        let help_text = Line::from(vec![
-            Span::raw("Up/Down: Navigate  "),
-            Span::raw("Enter: Open/Edit  "),
-            Span::raw("n: New Note  "),
-            Span::raw("q: Quit"),
-        ]);
-        let help = Paragraph::new(help_text).block(Block::default().borders(Borders::ALL));
-        f.render_widget(help, chunks[2]);
-    })?;
+    tui::run_tui(&mut config, &pin, &mut note_database, &args)?;
 
     Ok(())
 }
@@ -258,22 +85,20 @@ fn encrypt_unencrypted_files(notes_dir: impl AsRef<Path>, pin: &str) -> Result<(
             // try to decrypt to check if it's an encrypted file that just doesn't have the .enc.txt extension
             match file::load_and_decrypt_note_content(path.to_string_lossy().as_ref(), pin) {
                 Ok(_) => {
-                    // it's an encrypted file, but without the correct extension, so rename it
+                    // rename it: it's an encrypted file, but without the correct extension
                     let new_path = path.with_extension("enc.txt");
                     println!("Renaming encrypted file: {:?} -> {:?}", path, new_path);
                     fs::rename(&path, &new_path)?;
                 }
                 Err(_) => {
-                    // it's genuinely unencrypted or corrupted, add to list
+                    // it's truly unencrypted or corrupted, so add to list
                     unencrypted_files.push(path);
                 }
             }
         }
     }
 
-    if unencrypted_files.is_empty() {
-        println!("No unencrypted files found.");
-    } else {
+    if !unencrypted_files.is_empty() {
         println!(
             "Found {} unencrypted files. Encrypting...",
             unencrypted_files.len()
@@ -289,7 +114,9 @@ fn encrypt_unencrypted_files(notes_dir: impl AsRef<Path>, pin: &str) -> Result<(
             println!("Encrypted {:?} to {:?}", file_path, new_path);
         }
         println!("Encryption complete.");
+        return Ok(());
     }
 
+    println!("No unencrypted files found.");
     Ok(())
 }
